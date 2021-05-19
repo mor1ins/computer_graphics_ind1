6c199f3069d6db0306f2210d2d76d855f7c54732
Eduard Kuliev <36066152+mor1ins@users.noreply.github.com>
2021-05-19 21:18:38 +0300

    lab5 ready

--- script.js
+++ script.js
@@ -11,12 +11,20 @@ const sceneState = {
     lightShininess: NaN,
     shading: NaN,
     lightModel: NaN,
+
+    colorCoef: NaN,
+    textureCoef: NaN,
+    materialCoef: NaN,
 }
 
 
 function updateState() {
     sceneState.lightPower = parseFloat(document.querySelector('#lightPower').value);
     sceneState.dampingFunction = parseInt(document.querySelector('.dampingFunction').value)
+
+    sceneState.colorCoef = parseFloat(document.querySelector('#colorCoef').value);
+    sceneState.textureCoef = parseFloat(document.querySelector('#textureCoef').value);
+    sceneState.materialCoef = parseFloat(document.querySelector('#materialCoef').value);
 
     const x = parseInt(document.querySelector('#x_coord').value);
     const y = parseInt(document.querySelector('#y_coord').value);
@@ -28,17 +36,6 @@ function updateState() {
     sceneState.shading = parseInt(document.querySelector('.shading').value)
     sceneState.lightModel = parseInt(document.querySelector('.lightModel').value)
 }
-
-
-// window.addEventListener('keydown', function (event)
-// {
-//     if (event.key === '1') {
-//         sceneState.dampingFunction = 0;
-//     }
-//     else if (event.key === '2') {
-//         sceneState.dampingFunction = 1;
-//     }
-// });
 
 
 class Drawable {
@@ -55,6 +52,31 @@ class Drawable {
         this.colors = NaN;
         this.triangles = NaN;
         this.normals = NaN;
+        this.textureCoordinates = NaN;
+        this.texture = NaN;
+        this.material = NaN;
+    }
+
+    setBuffers() {
+        this.positionBuffer = this.gl.createBuffer();
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.positionBuffer);
+        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(this.positions), this.gl.STATIC_DRAW);
+
+        this.triangleBuffer = this.gl.createBuffer();
+        this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.triangleBuffer);
+        this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(this.triangles), this.gl.STATIC_DRAW);
+
+        this.colorBuffer = this.gl.createBuffer();
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.colorBuffer);
+        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(this.colors), this.gl.STATIC_DRAW);
+
+        this.normalBuffer = this.gl.createBuffer();
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.normalBuffer);
+        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(this.normals), this.gl.STATIC_DRAW);
+
+        this.textureCoordinatesBuffer = this.gl.createBuffer();
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.textureCoordinatesBuffer);
+        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(this.textureCoordinates), this.gl.STATIC_DRAW);
     }
 
     getBuffers() {
@@ -110,6 +132,32 @@ class Drawable {
         this.gl.enableVertexAttribArray(programInfo.attribLocations.normal);
     }
 
+    setTextureCoordinate(programInfo) {
+        const numComponents = 2;
+        const type = this.gl.FLOAT;
+        const normalize = false;
+        const stride = 0;
+        const offset = 0;
+
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.textureCoordinatesBuffer);
+        this.gl.vertexAttribPointer(
+            programInfo.attribLocations.textureCoordinate,
+            numComponents, type, normalize, stride, offset);
+        this.gl.enableVertexAttribArray(programInfo.attribLocations.textureCoordinate);
+    }
+
+    setTexture(programInfo) {
+        this.gl.activeTexture(this.gl.TEXTURE0);
+        this.gl.bindTexture(this.gl.TEXTURE_2D, this.texture);
+        this.gl.uniform1i(programInfo.uniformLocations.texture, 0);
+    }
+
+    setMaterial(programInfo) {
+        this.gl.activeTexture(this.gl.TEXTURE1);
+        this.gl.bindTexture(this.gl.TEXTURE_2D, this.material);
+        this.gl.uniform1i(programInfo.uniformLocations.material, 1);
+    }
+
     to_position(modelViewMatrix) {
         this.translate(modelViewMatrix, this.position);
     }
@@ -133,6 +181,47 @@ class Drawable {
         this.translate(modelViewMatrix, translation.map(p => -p));
         this.rotate(modelViewMatrix, rad, axis);
         this.translate(modelViewMatrix, translation);
+    }
+
+    loadTexture(gl, url) {
+        const texture = gl.createTexture();
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+
+        const level = 0;
+        const internalFormat = gl.RGBA;
+        const width = 1;
+        const height = 1;
+        const border = 0;
+        const srcFormat = gl.RGBA;
+        const srcType = gl.UNSIGNED_BYTE;
+        const pixel = new Uint8Array([0, 0, 255, 255]);  // непрозрачный синий
+        gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+            width, height, border, srcFormat, srcType,
+            pixel);
+
+        function isPowerOf2(value) {
+            return (value & (value - 1)) === 0;
+        }
+
+        const image = new Image();
+        image.onload = function () {
+            gl.bindTexture(gl.TEXTURE_2D, texture);
+            gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+            gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, srcFormat, srcType, image);
+
+            if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+                gl.generateMipmap(gl.TEXTURE_2D);
+            } else {
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+            }
+        };
+
+        image.crossOrigin = '';
+        image.src = url;
+
+        return texture;
     }
 }
 
@@ -188,28 +277,13 @@ class Sphere extends Drawable {
         let center = [0, 0, 0];
         this.normals = this.positions.map((v, i) => v - center[i % center.length]);
 
-
-        this.positionBuffer = this.gl.createBuffer();
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.positionBuffer);
-        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(this.positions), this.gl.STATIC_DRAW);
-
-        this.triangleBuffer = this.gl.createBuffer();
-        this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.triangleBuffer);
-        this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(this.triangles), this.gl.STATIC_DRAW);
-
-        this.colorBuffer = this.gl.createBuffer();
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.colorBuffer);
-        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(this.colors), this.gl.STATIC_DRAW);
-
-        this.normalBuffer = this.gl.createBuffer();
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.normalBuffer);
-        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(this.normals), this.gl.STATIC_DRAW);
+        this.setBuffers();
     }
 }
 
 
 class Cube extends Drawable {
-    constructor(webgl_context, size, color, default_position=[0.0, 0.0, 0.0]) {
+    constructor(webgl_context, size, color, texture_url, material_url, default_position=[0.0, 0.0, 0.0]) {
         super(webgl_context);
 
         this.positions = ([
@@ -252,10 +326,6 @@ class Cube extends Drawable {
 
         this.position = default_position;
 
-        this.positionBuffer = this.gl.createBuffer();
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.positionBuffer);
-        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(this.positions), this.gl.STATIC_DRAW);
-
         const faceColors = [
             [...color,  1.0],    // Front face: white
             [...color,  1.0],    // Back face: red
@@ -267,11 +337,6 @@ class Cube extends Drawable {
 
         this.colors = [].concat.apply([], faceColors.map(color => [...color, ...color, ...color, ...color]));
 
-        this.colorBuffer = this.gl.createBuffer();
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.colorBuffer);
-        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(this.colors), this.gl.STATIC_DRAW);
-
-
         this.triangles = [
             0,  1,  2,      0,  2,  3,    // front
             4,  5,  6,      4,  6,  7,    // back
@@ -280,12 +345,6 @@ class Cube extends Drawable {
             16, 17, 18,     16, 18, 19,   // right
             20, 21, 22,     20, 22, 23,   // left
         ];
-
-        this.triangleBuffer = this.gl.createBuffer();
-        this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.triangleBuffer);
-        this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER,
-            new Uint16Array(this.triangles), this.gl.STATIC_DRAW);
-
 
         this.normals = [
             [0, 0, 1],    // front
@@ -298,9 +357,43 @@ class Cube extends Drawable {
 
         this.normals = [].concat.apply([], this.normals.map(n => [...n, ...n, ...n, ...n]));
 
-        this.normalBuffer = this.gl.createBuffer();
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.normalBuffer);
-        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(this.normals), this.gl.STATIC_DRAW);
+        this.textureCoordinates = [
+            // Front
+            0.0,  0.0,
+            1.0,  0.0,
+            1.0,  1.0,
+            0.0,  1.0,
+            // Back
+            0.0,  0.0,
+            1.0,  0.0,
+            1.0,  1.0,
+            0.0,  1.0,
+            // Top
+            0.0,  0.0,
+            1.0,  0.0,
+            1.0,  1.0,
+            0.0,  1.0,
+            // Bottom
+            0.0,  0.0,
+            1.0,  0.0,
+            1.0,  1.0,
+            0.0,  1.0,
+            // Right
+            0.0,  0.0,
+            1.0,  0.0,
+            1.0,  1.0,
+            0.0,  1.0,
+            // Left
+            0.0,  0.0,
+            1.0,  0.0,
+            1.0,  1.0,
+            0.0,  1.0,
+        ];
+
+        this.texture = this.loadTexture(this.gl, texture_url);
+        this.material = this.loadTexture(this.gl, material_url);
+
+        this.setBuffers();
     }
 }
 
@@ -320,8 +413,17 @@ class Scene {
                 vertexPosition: this.gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
                 vertexColor: this.gl.getAttribLocation(shaderProgram, 'aVertexColor'),
                 normal: this.gl.getAttribLocation(shaderProgram, 'aNormal'),
+                textureCoordinate: this.gl.getAttribLocation(shaderProgram, 'aTextureCoordinate'),
             },
             uniformLocations: {
+                textureMatrix: this.gl.getUniformLocation(shaderProgram, 'uTextureMatrix'),
+                texture: this.gl.getUniformLocation(shaderProgram, 'uTexture'),
+                material: this.gl.getUniformLocation(shaderProgram, 'uMaterial'),
+
+                colorCoef: this.gl.getUniformLocation(shaderProgram, 'uColorCoef'),
+                textureCoef: this.gl.getUniformLocation(shaderProgram, 'uTextureCoef'),
+                materialCoef: this.gl.getUniformLocation(shaderProgram, 'uMaterialCoef'),
+
                 projectionMatrix: this.gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
                 modelViewMatrix: this.gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
 
@@ -340,10 +442,10 @@ class Scene {
         };
 
         this.objects = [
-            new Cube(this.gl, 1, [1, 0.84, 0], [0, -5, -20]), //gold
-            new Cube(this.gl, 1, [1, 0.84, 0], [0, 1.5, -7]), // gold
-            new Cube(this.gl, 1, [0.75, 0.75, 0.75], [-4, -0.5, -11]), //silver
-            new Sphere(this.gl, 5, [0.8, 0.5, 0.2], [3, -0.5, -9]), // bronze
+            new Cube(this.gl, 1, [1, 0.84, 0], './1.jpg', './wood.jpg', [0, -5, -20]), //gold
+            new Cube(this.gl, 1, [1, 0.84, 0], './2.png', './wood.jpg', [0, 1.5, -7]), // gold
+            new Cube(this.gl, 1, [0.75, 0.75, 0.75], './3.jpg', './wood.jpg', [-4, -0.5, -11]), //silver
+            // new Sphere(this.gl, 5, [0.8, 0.5, 0.2], [3, -0.5, -9]), // bronze
         ];
 
         this.then = 0;
@@ -399,6 +501,9 @@ class Scene {
             obj.setVertexPositions(this.programInfo);
             obj.setVertexColors(this.programInfo);
             obj.setNormals(this.programInfo);
+            obj.setTextureCoordinate(this.programInfo);
+            obj.setTexture(this.programInfo);
+            obj.setMaterial(this.programInfo);
 
             const buffers = obj.getBuffers();
 
@@ -415,6 +520,10 @@ class Scene {
 
             this.gl.uniform1i(this.programInfo.uniformLocations.lightModel, this.state.lightModel);
             this.gl.uniform1i(this.programInfo.uniformLocations.shading, this.state.shading);
+
+            this.gl.uniform1f(this.programInfo.uniformLocations.colorCoef, this.state.colorCoef);
+            this.gl.uniform1f(this.programInfo.uniformLocations.textureCoef, this.state.textureCoef);
+            this.gl.uniform1f(this.programInfo.uniformLocations.materialCoef, this.state.materialCoef);
 
             this.gl.drawElements(this.gl.TRIANGLES, buffers.raw_indices.length, this.gl.UNSIGNED_SHORT, 0);
 
@@ -479,9 +588,17 @@ function main() {
         uniform lowp int uLightModel;
         uniform float uLightShininess;
         
+        uniform float uColorCoef;
+        uniform float uTextureCoef;
+        uniform float uMaterialCoef;
+        
+        uniform sampler2D uTexture;
+        uniform sampler2D uMaterial;
+        
         varying vec4 vPosition;
         varying vec4 vColor;
         varying vec3 vNormal;
+        varying vec2 vTextureCoordinate;
     `;
 
     const commonFunctions = `
@@ -573,6 +690,7 @@ function main() {
     attribute vec4 aVertexPosition;
     attribute vec4 aVertexColor;
     attribute vec3 aNormal;
+    attribute vec2 aTextureCoordinate;
     
     ${commonVariables}
     
@@ -595,6 +713,7 @@ function main() {
         vColor.rgb *= light;
         vPosition = aVertexPosition;
         vNormal = normal;
+        vTextureCoordinate = aTextureCoordinate;
     }
     `;
 
@@ -616,10 +735,14 @@ function main() {
             lightDirection, positionEye3, uLightPower, uLightShininess);
         light = dampLight(uDampingFunction, light);
         
-        gl_FragColor = vColor;
+        float total = uColorCoef + uTextureCoef + uMaterialCoef;
+        
+        gl_FragColor = vec4(
+            vec3(vColor) * uColorCoef / total 
+            + vec3(texture2D(uTexture, vTextureCoordinate)) * uTextureCoef / total
+            + vec3(texture2D(uMaterial, vTextureCoordinate)) * uMaterialCoef / total, 1.0);
         gl_FragColor.rgb *= light;
-    }
-    `;
+    }`;
 
     const scene = new Scene(gl, vsSource, fsSource, sceneState);
     scene.start();
